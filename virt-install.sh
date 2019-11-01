da83e4a82cf0b3af308c8d7471dd385cced59e53
Cplus Shen <cplus.shen@gmail.com>
2019-11-01 20:04:24 +0800

    added ubuntu18.04 support for virt-install.sh

--- virt-install.sh
+++ virt-install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -xe
 # Date: 2017-0516
 # Author: cplus.shen
-# Description: setup M30 chip passthrough mode and launch CentOS 7.2 VM
 
 KVM_VM_NAME=VM
 set +e 
@@ -9,12 +8,14 @@ virsh destroy  $KVM_VM_NAME
 virsh undefine $KVM_VM_NAME
 set -e
 virt-install \
-    --disk=$1,device=disk,bus=virtio,format=qcow2 \
+    --arch x86_64 \
     --ram 2048 \
     --vcpus 2 \
-    --name $KVM_VM_NAME \
-    --network bridge=br0 \
+    --os-variant ubuntu18.04 \
+    --disk=$1,device=disk,bus=virtio,format=qcow2 \
     --import \
     --vnc \
-    --noautoconsole
+    --noautoconsole  \
+    --network bridge=br0 \
+    --name $KVM_VM_NAME
 
