de22d6cb56350cbb9ed0a46ead5a8d5a0fffd4f6
Cplus Shen <cplus.shen@gmail.com>
2019-11-04 09:31:08 +0000

    added apt-install for install package for ifupdown and vlan

---
+++ apt-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -xe
+# Date: Nov 04, 2019
+# Author: cplus.shen
+# Description: Install debian based package on VM image
+
+virt-customize -v -x --run-command 'sudo apt install -y ifupdown; sudo apt install -y vlan' -a $1
