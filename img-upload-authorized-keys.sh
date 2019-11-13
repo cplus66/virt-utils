bc1eb34475c5864867b1f083cf388e847765c233
Cplus Shen <cplus.shen@gmail.com>
2019-11-13 05:28:09 +0000

    added restorecon authorized_keys for centos

--- img-upload-authorized-keys.sh
+++ img-upload-authorized-keys.sh
@@ -2,20 +2,28 @@
 # Date: Nov 12, 2019
 # Author: cplus.shen
 # Description: upload Authorized keys
-# Usgae: img-upload-authorized-keys.sh image os_dist user_id
+# Usgae: img-upload-authorized-keys.sh image user_id
 #        image: image name
-#        os_dist: ubuntu | centos | debian
 #        user_id: cp | br | te | tt 
+# Environment:
+#        OS: ubuntu | centos | debian
 
-if [ $# -ne 3 ]; then
-  echo "Usgae: %0 image os_dist user_id"
+if [ $# -ne 2 ]; then
+  echo "Usgae: OS={centos|ubuntu} $0 image user_id"
   exit 1
 fi
 
+if [ "x$OS" == "x" ]; then
+  OS=ubuntu
+fi
+
 IMAGE_NAME=$1
-OS_DIST=$2
-USER_ID=$3
+USER_ID=$2
 
 BASEDIR=$(dirname "$0")
 
-virt-customize -a $IMAGE_NAME  --upload $BASEDIR/public-keys/$USER_ID.pub:/home/$OS_DIST/.ssh/authorized_keys
+sudo virt-customize -a $IMAGE_NAME  --upload $BASEDIR/public-keys/$USER_ID.pub:/home/$OS/.ssh/authorized_keys
+
+if [ $OS == "centos" ]; then
+  sudo virt-customize -a $IMAGE_NAME  --run-command "chmod 600 /home/centos/.ssh/authorized_keys && restorecon /home/centos/.ssh/authorized_keys" 
+fi
