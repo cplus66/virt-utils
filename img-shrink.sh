5b975e6467ca59eb5e88ecdf682eed8c2e29b428
Cplus Shen <cplus.shen@gmail.com>
2019-12-23 02:46:49 +0000

    added img-shrink.sh parameter sanity check

--- img-shrink.sh
+++ img-shrink.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# Date: Dec 23, 2019
+# Author: cplus.shen
+# Description: shrink the image size
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <image name>"
+  exit 1 
+fi
+
 mv $1 $1.backup
 sudo qemu-img convert -O qcow2 -c $1.backup $1
 rm -f $1.backup
