2c1ed6501abd56532ec60ec69f45f5332c00ac6e
Cplus Shen <cplus.shen@gmail.com>
2020-04-22 11:46:32 +0800

    improved img-resize script wihtout running parted, fdisk and resize2fs in VM

--- img-resize.sh
+++ img-resize.sh
@@ -2,7 +2,7 @@
 # Date: Nov 12, 2019
 # Author: cplus.shen
 # Description: virtual disk resize
-# Usage: img-resize.sh image partition size
+# Usage: OS=ubuntu img-resize.sh image partition size
 #          image: image name
 #          partition: /dev/sda1 
 #          size: 16G
@@ -12,14 +12,20 @@
 # - Run resize2fs /dev/vda1
 
 if [ $# -ne 2 ]; then
-  echo "$0 image size"
+  echo "OS=ubuntu $0 image size"
   exit 1
 fi
 
 IMAGE=$1
 SIZE=$2
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 sudo qemu-img resize $IMAGE $SIZE
-echo "ubuntu: run parted, fdisk and resize2fs"
-echo "centos: run parted and fdisk"
 
+if [ x$OS == "xubuntu" ]; then
+  #run parted, fdisk and resize2fs"
+  sudo virt-customize -a $IMAGE --upload $DIR/img-resize-sda.exp:/root
+  sudo virt-customize -a $IMAGE --run-command "/root/img-resize-sda.exp; rm /root/img-resize-sda.exp"
+else
+  echo "centos: run parted and fdisk"
+fi
