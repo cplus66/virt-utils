498e11261c8311cb09832ec83e33209c9881b14b
Cplus Shen <cplus.shen@gmail.com>
2020-07-16 15:15:18 +0800

    create user folder if it doesn't exist.

--- img-upload-authorized-keys.sh
+++ img-upload-authorized-keys.sh
@@ -21,10 +21,12 @@ IMAGE_NAME=$1
 USER_ID=$2
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-sudo virt-customize -a $IMAGE_NAME  --upload $BASEDIR/public-keys/$USER_ID.pub:/home/$OS/.ssh/authorized_keys
 
 if [ $OS == "centos" ]; then
+  sudo virt-customize -a $IMAGE_NAME  --upload $BASEDIR/public-keys/$USER_ID.pub:/home/$OS/.ssh/authorized_keys
   sudo virt-customize -a $IMAGE_NAME  --run-command "chmod 600 /home/centos/.ssh/authorized_keys && restorecon /home/centos/.ssh/authorized_keys" 
 else # ubuntu and debian
+  sudo virt-customize -a $IMAGE_NAME  --run-command "mkdir -p /home/$OS/.ssh"
+  sudo virt-customize -a $IMAGE_NAME  --upload $BASEDIR/public-keys/$USER_ID.pub:/home/$OS/.ssh/authorized_keys
   sudo virt-customize -a $IMAGE_NAME  --run-command "chmod 600 /home/$OS/.ssh/authorized_keys" 
 fi
