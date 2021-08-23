3c76076ae604142f747a781107e87e9991185a54
Cplus Shen <cplus.shen@gmail.com>
2021-08-23 09:08:00 +0800

    added jetpack-4 to VM.

--- docker-2-virsh-start.sh
+++ docker-2-virsh-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-LIST="esp-3000-ubu14-0 kernel yocto-4 vega-3500-sc-0 esp-9010-deb8-0"
+LIST="esp-3000-ubu14-0 kernel yocto-4 vega-3500-sc-0 esp-9010-deb8-0 openssl-0 openssl-1 openssl-2 centos-7.9 jetpack-4"
 
 for i in $LIST; do
   virsh start $i
