cad7ed0575a12f599be432e29ea0e0f0ee535a6e
Cplus Shen <cplus.shen@gmail.com>
2021-08-23 09:02:40 +0800

    added jetpack-1 to VMs

--- kvm-1-virsh-start.sh
+++ kvm-1-virsh-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-LIST="gw dio-yocto-0 esp-3000-ubu14-1 esp-9010-0 yocto-0 bcn-ipv6-0"
+LIST="gw dio-yocto-0 esp-3000-ubu14-1 esp-9010-0 yocto-0 bcn-ipv6-0 jetpack-1"
 
 for i in $LIST; do
   virsh start $i
