fa64c49b59c15971768d98889b291e1580cdc11f
Cplus Shen <cplus.shen@gmail.com>
2021-08-23 09:06:31 +0800

    added jetpack-3 to VM.

--- docker-1-virsh-start.sh
+++ docker-1-virsh-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-LIST="esp-9010-deb10-1 yocto-3 bcn-ipv6-1 esp-9010b-1 esp-9010b-2 vega-3500-3"
+LIST="esp-9010-deb10-1 yocto-3 bcn-ipv6-1 esp-9010b-1 esp-9010b-2 vega-3500-3 jetpack-3"
 
 for i in $LIST; do
   virsh start $i
