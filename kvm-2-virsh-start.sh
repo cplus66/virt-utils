09c5ea10fdb8779248d9466691288fcf4b239dc3
Cplus Shen <cplus.shen@gmail.com>
2021-08-23 09:03:52 +0800

    added jetpack-2 to VM.

--- kvm-2-virsh-start.sh
+++ kvm-2-virsh-start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-LIST="dio-yocto-1 esp-9010-0-debian10 esp-9010b yocto-2 esp-9400-terryt esp-9010-1"
+LIST="dio-yocto-1 esp-9010-0-debian10 esp-9010b yocto-2 esp-9400-terryt esp-9010-1 jetpack-2"
 
 for i in $LIST; do
   virsh start $i
